11a1050162e0daa4f299aab9447cae946816c59b
Steve Hull <hullstevenj@gmail.com>
2024-08-06 16:21:25 -0700

    Added stronger password protection using .env and cleaning up code

--- tooling_inventory.py
+++ tooling_inventory.py
@@ -1,18 +1,28 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 
-auth = HTTPBasicAuth()
 
+load_dotenv()
+
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 app.secret_key = "tarfu"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mariadb+mariadbconnector://mysqluser:K0m84t@127.0.0.1:3306/tooling'
+db_username = os.getenv('DB_USERNAME')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
 
 class Cabinet_Tooling(db.Model):
     __tablename__ = "cabinet_tooling"
@@ -36,10 +46,11 @@ class Cabinet_Tooling(db.Model):
 
 @auth.verify_password
 def verify_password(username, password):
-    user = 'sifu'
-    pw = '800g3r'
+    # Verify the admin username and password for table inserts and updates
+    user = os.getenv('ADMIN_USERNAME')
+    password = os.getenv('ADMIN_PASSWORD')
     users = {
-        user: generate_password_hash(pw)
+        user: generate_password_hash(password)
     }
     if username in users:
         return check_password_hash(users.get(username), password)
@@ -48,51 +59,52 @@ def verify_password(username, password):
 
 @app.route('/')
 def index():
+    print("index route accessed")
     tooling = Cabinet_Tooling.query.order_by(Cabinet_Tooling.category).all()
     nav_bar = Cabinet_Tooling.query.order_by(Cabinet_Tooling.category).all()
-
     return render_template('index.html', nav_bar=nav_bar, tooling=tooling)
 
-@app.route('/<cat_url>')
+
+@app.route('/category/<cat_url>')
 def category_url(cat_url):
+    print(f"category_url route accessed with cat_url: {cat_url}")
     tooling = Cabinet_Tooling.query.filter_by(category_url=cat_url).order_by(Cabinet_Tooling.category).all()
     nav_bar = Cabinet_Tooling.query.order_by(Cabinet_Tooling.category).all()
-
     return render_template('category.html', nav_bar=nav_bar, tooling=tooling)
 
 
 @app.route('/cat/<sub_cat_url>')
 def sub_category_url(sub_cat_url):
+    print("sub category")
     tooling = Cabinet_Tooling.query.filter_by(sub_category_url=sub_cat_url).order_by(Cabinet_Tooling.sub_category).all()
     nav_bar = Cabinet_Tooling.query.order_by(Cabinet_Tooling.category).all()
-
     return render_template('sub_category.html', nav_bar=nav_bar, tooling=tooling)
 
 
 @app.route('/sub/<ind_item>')
 def items_url(ind_item):
+    print("item")
     tooling = Cabinet_Tooling.query.filter_by(edp=ind_item).order_by(Cabinet_Tooling.edp).all()
     nav_bar = Cabinet_Tooling.query.order_by(Cabinet_Tooling.category).all()
-    
     return render_template('items.html', nav_bar=nav_bar, tooling=tooling)
 
 
 @app.route('/item/<product_code>', methods=['POST'])
 def remove_item(product_code):
+    print("remove item")
     product = Cabinet_Tooling.query.get(product_code)
     if product.quantity > 0:
         new_quantity = product.quantity - 1
         product.quantity = new_quantity
         db.session.commit()
-
     tooling = Cabinet_Tooling.query.filter_by(edp=product_code).order_by(Cabinet_Tooling.edp).all()
     nav_bar = Cabinet_Tooling.query.order_by(Cabinet_Tooling.category).all()
-
     return render_template('items.html', nav_bar=nav_bar, tooling=tooling)
 
 
 @app.route('/add/update>', methods=['POST'])
 def add_item():
+    print("add item")
     product_code = request.args.get('product_code')
     product_quantity = int(request.form.get('quantity'))
     if product_quantity > 0:
@@ -103,17 +115,21 @@ def add_item():
 
     tooling = Cabinet_Tooling.query.filter_by(edp=product_code).order_by(Cabinet_Tooling.edp).all()
     nav_bar = Cabinet_Tooling.query.order_by(Cabinet_Tooling.category).all()
-
     return render_template('items.html', nav_bar=nav_bar, tooling=tooling)
 
 
 @app.route('/prod/<product_code>')
 @auth.login_required
 def admin(product_code):
+    print("admin")
     tooling = Cabinet_Tooling.query.filter_by(edp=product_code).order_by(Cabinet_Tooling.edp).all()
     nav_bar = Cabinet_Tooling.query.order_by(Cabinet_Tooling.category).all()
-    
     return render_template('update_item.html', nav_bar=nav_bar, tooling=tooling)
+
+
+#@app.route('/newitem', methods=['POST'])
+#@auth.login_required
+#def new_product():
 
 
 
